0089c25a205f6f5c509fd046962b7c20532d9f2f
Pedram Nazari <13958275+pedramnazari@users.noreply.github.com>
2024-07-11 15:28:58 +0200

    removed java module and added ArchUnit dependencies

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module de.pedramnazari.simpletbg {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-    requires org.controlsfx.controls;
-    requires java.logging;
-
-    exports de.pedramnazari.simpletbg.ui.view;
-}
