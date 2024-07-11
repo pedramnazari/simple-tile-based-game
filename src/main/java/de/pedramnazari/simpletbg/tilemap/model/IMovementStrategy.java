4999ab9438418acabc4cf9d3c223722d58626095
Pedram Nazari <13958275+pedramnazari@users.noreply.github.com>
2024-07-11 16:30:39 +0200

    added ArchUnit-Tests and cleaning up the architecture

--- src/main/java/de/pedramnazari/simpletbg/tilemap/model/IMovementStrategy.java
+++ src/main/java/de/pedramnazari/simpletbg/tilemap/model/IMovementStrategy.java
@@ -1,59 +1,6 @@
 package de.pedramnazari.simpletbg.tilemap.model;
 
-import de.pedramnazari.simpletbg.tilemap.service.navigation.CollisionDetectionService;
-
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
-
 public interface IMovementStrategy {
 
-    // TODO: use GameContext instead of TileMap?
-
     Point calcNextMove(final TileMap tileMap, final IMovableTileElement element);
-
-    CollisionDetectionService getCollisionDetectionService();
-
-    default Set<Point> calcValidMovePositionsWithinMap(TileMap tileMap, int currentX, int currentY) {
-        final Set<Point> validPositions = new HashSet<>();
-        for (MoveDirection moveDirection : MoveDirection.values()) {
-            Point newPosition = calcValidMovePositionWithinMapForDirection(tileMap, currentX, currentY, moveDirection).orElse(null);
-
-            if (newPosition != null) {
-                validPositions.add(newPosition);
-            }
-        }
-
-        return validPositions;
-    }
-
-    default Optional<Point> calcValidMovePositionWithinMapForDirection(TileMap tileMap, int currentX, int currentY, MoveDirection moveDirection) {
-        final Point newPosition = calcNewPosition(moveDirection, currentX, currentY);
-
-        if (isPositionWithinBoundsOfMap(tileMap, newPosition.getX(), newPosition.getY())) {
-            if (!getCollisionDetectionService().isCollisionWithObstacle(tileMap, newPosition.getX(), newPosition.getY())) {
-                return Optional.of(newPosition);
-            }
-        }
-
-        return Optional.empty();
-    }
-
-    default Point calcNewPosition(final MoveDirection moveDirection, int oldX, int oldY) {
-        int dx = 0;
-        int dy = 0;
-
-        switch (moveDirection) {
-            case UP -> dy = -1;
-            case DOWN -> dy = 1;
-            case LEFT -> dx = -1;
-            case RIGHT -> dx = 1;
-        }
-
-        return new Point(oldX + dx, oldY + dy);
-    }
-
-    default boolean isPositionWithinBoundsOfMap(final TileMap tileMap, final int newX, final int newY) {
-        return (newX >= 0) && (newX < tileMap.getWidth()) && (newY >= 0) && (newY < tileMap.getHeight());
-    }
 }
--- src/main/java/de/pedramnazari/simpletbg/tilemap/service/navigation/AbstractMovementStrategy.java
+++ src/main/java/de/pedramnazari/simpletbg/tilemap/service/navigation/AbstractMovementStrategy.java
@@ -1,18 +1,63 @@
 package de.pedramnazari.simpletbg.tilemap.service.navigation;
 
-import de.pedramnazari.simpletbg.tilemap.model.IMovementStrategy;
+import de.pedramnazari.simpletbg.tilemap.model.*;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public abstract class AbstractMovementStrategy implements IMovementStrategy {
 
     private final CollisionDetectionService collisionDetectionService;
 
+    @Override
+    public abstract Point calcNextMove(final TileMap tileMap, final IMovableTileElement element);
+
     public AbstractMovementStrategy(CollisionDetectionService collisionDetectionService) {
         this.collisionDetectionService = collisionDetectionService;
     }
 
-    @Override
-    public CollisionDetectionService getCollisionDetectionService() {
-        return collisionDetectionService;
+    protected Set<Point> calcValidMovePositionsWithinMap(TileMap tileMap, int currentX, int currentY) {
+        final Set<Point> validPositions = new HashSet<>();
+        for (MoveDirection moveDirection : MoveDirection.values()) {
+            Point newPosition = calcValidMovePositionWithinMapForDirection(tileMap, currentX, currentY, moveDirection).orElse(null);
+
+            if (newPosition != null) {
+                validPositions.add(newPosition);
+            }
+        }
+
+        return validPositions;
+    }
+
+    protected Optional<Point> calcValidMovePositionWithinMapForDirection(TileMap tileMap, int currentX, int currentY, MoveDirection moveDirection) {
+        final Point newPosition = calcNewPosition(moveDirection, currentX, currentY);
+
+        if (isPositionWithinBoundsOfMap(tileMap, newPosition.getX(), newPosition.getY())) {
+            if (!collisionDetectionService.isCollisionWithObstacle(tileMap, newPosition.getX(), newPosition.getY())) {
+                return Optional.of(newPosition);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    protected Point calcNewPosition(final MoveDirection moveDirection, int oldX, int oldY) {
+        int dx = 0;
+        int dy = 0;
+
+        switch (moveDirection) {
+            case UP -> dy = -1;
+            case DOWN -> dy = 1;
+            case LEFT -> dx = -1;
+            case RIGHT -> dx = 1;
+        }
+
+        return new Point(oldX + dx, oldY + dy);
+    }
+
+    protected boolean isPositionWithinBoundsOfMap(final TileMap tileMap, final int newX, final int newY) {
+        return (newX >= 0) && (newX < tileMap.getWidth()) && (newY >= 0) && (newY < tileMap.getHeight());
     }
 
 }
